08e8d9eb063c0f21e077921140b6c7b184124795
Kyle Moore <kbm190@icloud.com>
2023-10-15 23:21:28 -0500

    sales report

--- lab5.c
+++ lab5.c
@@ -24,8 +24,93 @@ void calculateCombinations(int score)
     }
 }
 
+void printSalesReport(double sales[], char *months[])
+{
+    printf("Monthly sales report for 2022:\n");
+    printf("Month\tSales\n");
+    for (int i = 0; i < 12; i++)
+    {
+        printf("%s\t%.2f\n", months[i], sales[i]);
+    }
+}
+
+void printSalesSummary(double sales[])
+{
+    double min = sales[0];
+    double max = sales[0];
+    double total = 0;
+
+    for (int i = 1; i < 12; i++)
+    {
+        if (sales[i] < min)
+        {
+            min = sales[i];
+        }
+        if (sales[i] > max)
+        {
+            max = sales[i];
+        }
+        total += sales[i];
+    }
+
+    double avg = total / 12;
+
+    printf("\nSales summary:\n");
+    printf("Minimum sales: $%.2f\n", min);
+    printf("Maximum sales: $%.2f\n", max);
+    printf("Average sales: $%.2f\n", avg);
+}
+
+void printMovingAverages(double sales[])
+{
+    printf("\nSix-Month Moving Average Report:\n");
+    for (int i = 0; i < 7; i++)
+    {
+        double average = (sales[i] + sales[i + 1] + sales[i + 2] + sales[i + 3] + sales[i + 4] + sales[i + 5]) / 6;
+        printf("Month %d - %d: $%.2f\n", i + 1, i + 6, average);
+    }
+}
+
+void sortSales(double sales[], char *months[])
+{
+    for (int i = 0; i < 11; i++)
+    {
+        for (int j = i + 1; j < 12; j++)
+        {
+            if (sales[i] < sales[j])
+            {
+                double temp_sales = sales[i];
+                sales[i] = sales[j];
+                sales[j] = temp_sales;
+                char *temp_month = months[i];
+                months[i] = months[j];
+                months[j] = temp_month;
+            }
+        }
+    }
+
+    printf("\nSales Report (Highest to Lowest):\n");
+    printf("Month\tSales\n");
+    for (int i = 0; i < 12; i++)
+    {
+        printf("%s\t%.2f\n", months[i], sales[i]);
+    }
+}
+
 int main()
 {
+    double sales[] = {
+        23458.01, 40112.00, 56011.85, 37820.88, 37904.67, 60200.22,
+        72400.31, 56210.89, 67230.84, 68233.12, 80950.34, 95225.22};
+    char *months[] = {
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"};
+    printf("Problem 1:\n");
+    printSalesReport(sales, months);
+    printSalesSummary(sales);
+    printMovingAverages(sales);
+    sortSales(sales, months);
+    printf("\nProblem 2:\n"); // sales report first then football scores
     int score;
     printf("Enter 0 or 1 to stop\n"); // these scores are invalid
     while (1)
